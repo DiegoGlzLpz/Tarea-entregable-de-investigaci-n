de9581afa280b756ea49fc58140113fe9e996090
Diego <diego.glzlpz@gmail.com>
2026-04-20 22:44:45 +0200

    Volvemos a poner el main.py correctamente para que pase los tests

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 def comprobarPar(numero): #Funcion para comprobar si el numero es Par
     if (numero % 2 ==0):
-        return False
-    else:
         return True
+    else:
+        return False
     
 def quitarEspacios(texto):
-    return texto.replace(" ","-")
+    return texto.replace(" ","")
 
 def hacerCuadrado(numero):
-    return numero+numero
+    return numero*numero
 
 
